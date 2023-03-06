dea37b65132be729ed598e09c858ab190da6b1c7
mikecebul <mikecebul@gmail.com>
2023-03-05 20:56:35 -0500

    adjust font size hiearchy

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -12,82 +12,76 @@ const Projects = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8, delay: 0.3 }}
     >
-      <h2 className="pt-16 text-4xl font-semibold text-center">
+      <h2 className="pt-16 text-3xl font-semibold text-center">
         YouTube Videos
       </h2>
-      <div className="flex flex-col w-full pt-8">
+      <div className="flex flex-col w-full py-4">
         <div className="flex flex-col items-center">
           <Image
             className="object-cover w-full"
             src={Thumbnail}
             alt="YouTube video preview"
           />
-          <div className="p-8">
-            <p className="text-3xl font-semibold">DIY Wooden Card Box</p>
-            <p className="pt-4 text-lg">
+          <div className="p-8 bg-amber-200">
+            <p className="text-2xl font-semibold">DIY Wooden Card Box</p>
+            <p className="pt-4 text-lg font-light">
               Looking for rustic or boho wedding decor? Watch me turn three 1x4
               pine boards into a...
             </p>
             <div className="flex justify-center pt-4">
               <Link
                 href="#"
-                className="px-8 py-2 text-xl font-semibold border-4 border-black rounded-full"
+                className="px-8 py-2 border-2 border-black rounded-full font-lg semibold text-"
               >
                 See More
               </Link>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-col w-full pt-8">
-        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center">
           <Image
             className="object-cover w-full"
             src={Thumbnail}
             alt="YouTube video preview"
           />
-          <div className="p-8">
-            <p className="text-3xl font-semibold">DIY Wooden Card Box</p>
-            <p className="pt-4 text-lg">
+          <div className="p-8 bg-amber-200">
+            <p className="text-2xl font-semibold">DIY Wooden Card Box</p>
+            <p className="pt-4 text-lg font-light">
               Looking for rustic or boho wedding decor? Watch me turn three 1x4
               pine boards into a...
             </p>
             <div className="flex justify-center pt-4">
               <Link
                 href="#"
-                className="px-8 py-2 text-xl font-semibold border-4 border-black rounded-full"
+                className="px-8 py-2 border-2 border-black rounded-full font-lg semibold text-"
               >
                 See More
               </Link>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-col w-full pt-8">
-        <div className="flex flex-col items-center">
+        </div><div className="flex flex-col items-center">
           <Image
             className="object-cover w-full"
             src={Thumbnail}
             alt="YouTube video preview"
           />
-          <div className="p-8">
-            <p className="text-3xl font-semibold">DIY Wooden Card Box</p>
-            <p className="pt-4 text-lg">
+          <div className="p-8 bg-amber-200">
+            <p className="text-2xl font-semibold">DIY Wooden Card Box</p>
+            <p className="pt-4 text-lg font-light">
               Looking for rustic or boho wedding decor? Watch me turn three 1x4
               pine boards into a...
             </p>
             <div className="flex justify-center pt-4">
               <Link
                 href="#"
-                className="px-8 py-2 text-xl font-semibold border-4 border-black rounded-full"
+                className="px-8 py-2 border-2 border-black rounded-full font-lg semibold text-"
               >
                 See More
               </Link>
             </div>
           </div>
         </div>
+        </div>
       </div>
-      \
     </motion.div>
   );
 };
